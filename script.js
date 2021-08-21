fdeab21489c2e25a76f5e8617563cbacd76299fc
Yehor Sanko <egorsanko@gmail.com>
2021-08-21 16:54:56 +0200

    Mobile design

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var offset = $(".first-row").offset();
 $(window).scroll(function(event) {
   var st = $(this).scrollTop();
-  $(".first-row").css("left", -200 - st/10 - offset.left/10);
+  $(".first-row").css("left", -450 - st/10 - offset.left/10);
 });
 
 var offset = $(".second-row").offset();
 $(window).scroll(function(event) {
   var st = $(this).scrollTop();
-  $(".second-row").css("left", st/10 + offset.left/10);
+  $(".second-row").css("left", -1100 + st/10 + offset.left/10);
 });
